43a2be0521ef6e1256d080b6d8c7ca2084b93652
Todd Sayre <captain.friendly@gmail.com>
2025-04-22 23:40:28 -0400

    Update version number and add new menu commands for ticking checkboxes

--- mylar-ultron-import-checkbox-tick.user.js
+++ mylar-ultron-import-checkbox-tick.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mylar - Ultron Import Checkbox Ticker
 // @namespace    https://github.com/Sporkyy/userscripts
-// @version      2024.11.04
+// @version      2025.04.22.0
 // @description  Toggle import results by status in Mylar Ultron import page
 // @author       Sporkyy
 // @match        *://localhost:8090/*
@@ -20,6 +20,18 @@
   const qsaa = (...args) => [...qsa.apply(null, args)];
   const tick = box => (box.checked = true);
   const tickAll = (...boxes) => boxes.flat().forEach(tick);
+
+  GM_registerMenuCommand('✅ Tick All (Visible) Manual Intervention', () => {
+    const ccImported = qsaa('tr.Manual.Intervention input[type="checkbox"]');
+    console.log(`Ticking ${ccImported.length} checkboxes`);
+    tickAll(ccImported);
+  });
+
+  GM_registerMenuCommand('✅ Tick All (Visible) No Results', () => {
+    const ccImported = qsaa('tr.No.Results input[type="checkbox"]');
+    console.log(`Ticking ${ccImported.length} checkboxes`);
+    tickAll(ccImported);
+  });
 
   GM_registerMenuCommand('✅ Tick All (Visible) Imported', () => {
     const ccImported = qsaa('tr.Imported:not(.Not) input[type="checkbox"]');
